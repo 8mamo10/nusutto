a2c70b5e68eebf49a08a58efd9ff8e5163bcfd2b
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-09-19 11:14:01 +0700

    Import ireco_service

--- terraform/modules/cloudrun/ireco_service.tf
+++ terraform/modules/cloudrun/ireco_service.tf
@@ -2,4 +2,37 @@ resource "google_cloud_run_service" "ireco" {
   name     = "ireco"
   project  = var.project_id
   location = var.region
+
+  template {
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale" = "100"
+        "run.googleapis.com/client-name"   = "cloud-console"
+      }
+      labels = {}
+      name   = "ireco-00001-pay"
+    }
+
+    spec {
+      container_concurrency = 80
+      timeout_seconds       = 300
+
+      containers {
+        image = "asia-northeast2-docker.pkg.dev/nusutto/cloud-run-source-deploy/ireco"
+
+        ports {
+          container_port = 8080
+          name           = "http1"
+        }
+
+        resources {
+          limits = {
+            "cpu"    = "1000m"
+            "memory" = "512Mi"
+          }
+          requests = {}
+        }
+      }
+    }
+  }
 }
