e6c2633abacb2336a922c076b9dc8e9469d0e684
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-09-19 10:03:48 +0700

    Re-structure terraform

---
+++ terraform/environments/dev/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  project_id = "nusutto"
+
+  region = "asia-northeast1"
+  zone   = "asia-northeast1-c"
+}
--- terraform/main.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  credentials = "${file("credential.json")}"
-  project     = "${lookup(var.project_name, "${terraform.workspace}")}"
-  region      = "asia-northeast1"
-}
